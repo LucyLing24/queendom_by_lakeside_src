797b7b499d509f1635962e45fc575b0e227df527
LingLong <lucyling24@qq.com>
2023-06-05 00:51:23 +0800

    feat: change url

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ function Home() {
             <Row>
                 <Col md={3} className="home-header">
                     <h1 className="heading-name">
-                        Queendom Niche
+                        Queendom by Lakeside
                     </h1>
                     <p style={{textAlign: "justify"}}>
                         This project is an immersive game experience highlighting the last matriarchal clan society in China. Locating in Lugu Lake, Yunnan Province, Mosuo people follow the philosophy of coexistence. As a matriclan, the Mosuo continue their families through the practice of a dynamic and open marriage called “Walking Marriage.” When interacting with nature, this female-led clan emphasizes decentralization, which means people should follow the course of nature and utilize resources to a limited extent. Therefore, the Lugu Lake was praised as a paradise in China. However, fast-pacing urbanization and tourism disrupt the local environment and culture, leading to climate questions like the loss of the grass sea, shoreline encroachment, etc. Based on the VR engine, Queendom by the Lakeside would create a virtual world of Mosuo’s legend, in which users could experience the beauty of the Lugu River, the culture of Mosuo, and the way to interact with nature to reach a dynamic balance.
